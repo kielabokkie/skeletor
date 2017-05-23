8f90edce65787b022b1af1c1ebb14d45a23a67b1
Thijs Bouwes <thijsbouwes@gmail.com>
2017-05-24 09:38:05 +1200

    Added the projectfilesystem to the main Manager

--- src/Skeletor/Manager/Manager.php
+++ src/Skeletor/Manager/Manager.php
@@ -7,18 +7,22 @@ use League\Flysystem\Filesystem;
 abstract class Manager
 {
     protected $skeletorFilesystem;
+    protected $projectFilesystem;
     protected $options;
     protected $cli;
 
     /**
      * Manager constructor.
      * @param CLImate $cli
+     * @param Filesystem $projectFilesystem
+     * @param Filesystem $skeletorFilesystem
      * @param array $options
      */
-  public function __construct(CLImate $cli, Filesystem $skeletorFilesystem, array $options)
+    public function __construct(CLImate $cli, Filesystem $skeletorFilesystem, Filesystem $projectFilesystem, array $options)
     {
         $this->cli = $cli;
         $this->options = $options;
+        $this->projectFilesystem = $projectFilesystem;
         $this->skeletorFilesystem = $skeletorFilesystem;
     }
 }
