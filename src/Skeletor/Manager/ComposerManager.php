c3e90081bb59f755008db1feebc8d68ba775de11
kielabokkie <kielabokkie@gmail.com>
2017-09-19 16:08:54 +1200

    Keep things tidy in the composer file ✨

--- src/Skeletor/Manager/ComposerManager.php
+++ src/Skeletor/Manager/ComposerManager.php
@@ -24,7 +24,7 @@ class ComposerManager extends Manager implements InstallerInterface
      */
     public function preparePackageCommand(string $package, string $version, string $packageOptions)
     {
-        return sprintf('composer require %s %s %s', $package, $version, $packageOptions);
+        return sprintf('composer require %s %s %s --sort-packages', $package, $version, $packageOptions);
     }
 
     /**
@@ -34,7 +34,7 @@ class ComposerManager extends Manager implements InstallerInterface
     {
         $this->cli->yellow($command);
 
-        if($this->options['dryRun']) {
+        if ($this->options['dryRun']) {
             return;
         }
 
